fb6bf3bbe19df6552cadf4bc1950a1f09c58bd17
Oscar Martinez Sanchez <osanchez@crunchtime.com>
2024-07-08 15:58:44 -0600

    fixes

--- ds720+/config-drivers.sh
+++ ds720+/config-drivers.sh
@@ -1,18 +1,15 @@
-# executing as super user
-sudo su
-
 # loading modules related with USB serial
-for module in "usbserial ftdi_sio cdc-acm"; do
+for module in "usbserial" "ftdi_sio" "cdc-acm"; do
   echo "loading module: $module"
   modprobe $module
 done
 
 # load and copy drivers for sonoff zigbee device
-URL_BASE = "https://github.com/robertklep/dsm7-usb-serial-drivers/raw/main/modules/geminilake/dsm-7.2/"
+URL_BASE="https://github.com/robertklep/dsm7-usb-serial-drivers/raw/main/modules/geminilake/dsm-7.2/"
 # https://github.com/robertklep/dsm7-usb-serial-drivers/raw/main/modules/geminilake/dsm-7.2/rndis_host.ko
 cd /lib/modules
 
-for driver in "ch341 cp210x pl2303 rndis_host"; do
+for driver in "ch341" "cp210x" "pl2303" "rndis_host"; do
   if [-f "/lib/modules/${driver}.ko"]; then
     echo "file ${driver}.ko already exists, removing it..."
     rm -v "${driver}.ko"
@@ -27,3 +24,9 @@ done
 
 chmod 777 /dev/ttyUSB0
 chmod 777 /dev/ttyACM0
+
+for device in "ttyUSB0" "ttyACM0"; then
+  if [-f "/dev/${device}"]; then
+    chmod 777 "/dev/${device}"
+  fi
+done
